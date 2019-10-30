54da7ee42d278554a38aad22d297389291364899
koyger <koygerov.work@gmail.com>
2019-10-30 19:10:53 +0300

    Deleted unnecessary fixture from test_add_user.py

--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import *
 from model.user import *
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_user(app):
     app.session.login(username="admin", password="secret")
