d7d0e51f65f719a5b9ac9e68e56b0041d9964b05
koyger <koygerov.work@gmail.com>
2019-10-31 14:45:06 +0300

    Added Group Modify test

--- fixture/group.py
+++ fixture/group.py
@@ -25,6 +25,25 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
 
+    def modify(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init first group modification
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element_by_name("update").click()
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
---
+++ test/test_modify_group.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from model.group import *
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify(Group(name="MODIFIED group", header="MODIFIED 11", footer="MODIFIED 22"))
+    app.group.return_to_groups_page()
+    app.session.logout()
