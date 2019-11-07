0abe0d1292bec6025f27babd613d9676eedefacc
koyger <koygerov.work@gmail.com>
2019-11-07 19:19:42 +0300

    Group form entering modified and tested

--- fixture/group.py
+++ fixture/group.py
@@ -9,13 +9,15 @@ class GroupHelper:
 
     def fill_group(self, group):
         wd = self.app.wd
-        # fill group form
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def create(self, group):
         wd = self.app.wd
@@ -27,16 +29,20 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
 
-    def modify_first(self, group):
+    def modify_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # init first group modification
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("edit").click()
         # fill group form
         self.fill_group(group)
         # submit group creation
         wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
 
     def return_to_groups_page(self):
         wd = self.app.wd
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,6 +3,15 @@ from model.group import *
 
 def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first(Group(name="X MODIFIED group", header="MODIFIED 11", footer="MODIFIED 22"))
-    app.group.return_to_groups_page()
+    app.group.modify_first_group(Group(name="X MODIFIED group", header="MODIFIED 11", footer="MODIFIED 22"))
+    app.session.logout()
+
+def test_modify_first_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="X MODIFIED NAME group"))
+    app.session.logout()
+
+def test_modify_first_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="MODIFIED HEADER"))
     app.session.logout()
