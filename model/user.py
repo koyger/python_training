803bbd369f90ecefaacff3596ac698ab618568a1
koyger <koygerov.work@gmail.com>
2019-11-20 12:04:40 +0300

    modified class User added new repr, eq, id_or_max functions

--- model/user.py
+++ model/user.py
@@ -1,8 +1,26 @@
+from sys import maxsize
+
+
 class User:
 
-    def __init__(self, first_name, last_name, company_name,
-                 address):
+    def __init__(self, first_name=None, last_name=None, company_name=None,
+                 address=None, id=None):
         self.first_name = first_name
         self.last_name = last_name
         self.company_name = company_name
         self.address = address
+        self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.last_name, self.first_name)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and \
+               self.last_name == other.last_name and \
+               self.first_name == other.last_name
+
+    def id_or_max(cont):
+        if cont.id:
+            return int(cont.id)
+        else:
+            return maxsize
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -11,6 +11,7 @@ def test_modify_first_group(app):
     app.group.modify_first_group(group_to_modify)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    # вписываем модифицированную группу в первый элемент списка old_group
     old_groups[0] = group_to_modify
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
