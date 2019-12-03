d961940a6581401ab2c3bfc502984dd35f991bb1
koyger <koygerov.work@gmail.com>
2019-12-03 21:24:26 +0300

    Parametrized Add_group test is launched in cycle Also group deletion is launched in cycle so that group quantity would stay the same

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -10,15 +10,14 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [
-    Group(name=random_string("Group ", 10), header=random_string("Header ", 12), footer=random_string("Footer ", 12)),
-    Group(name="", header="", footer="")
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("Group ", 10), header=random_string("Header ", 12), footer=random_string("Footer ", 12))
+    for i in range(2)
 ]
 
 
 @pytest.mark.parametrize("group_data", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group_data):
-    # pass
     old_groups = app.group.get_group_list()
     group_to_add = Group(name="New group", header="11", footer="22")
     app.group.create(group_to_add)
@@ -27,14 +26,3 @@ def test_add_group(app, group_data):
     new_groups = app.group.get_group_list()
     old_groups.append(group_to_add)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_group_with_empty_fields(app):
-#     old_groups = app.group.get_group_list()
-#     group_to_add = Group(name="", header="", footer="")
-#     app.group.create(group_to_add)
-#     app.group.open_groups_page()
-#     assert len(old_groups) + 1 == app.group.count()
-#     new_groups = app.group.get_group_list()
-#     old_groups.append(group_to_add)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -4,25 +4,13 @@ from random import randrange
 
 
 def test_delete_some_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="FIRST group", header="11 first group", footer="22 first group"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []  # удалили первый элемент
-    assert old_groups == new_groups
-
-
-def test_delete_some_group_2(app):
-    # второе удаление сделано, чтобы не накапливались группы в списке
-    if app.group.count() == 0:
-        app.group.create(Group(name="FIRST group", header="11 first group", footer="22 first group"))
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    assert len(old_groups) - 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups[index:index+1] = []  # удалили первый элемент
-    assert old_groups == new_groups
+    for i in range(3):
+        if app.group.count() == 0:
+            app.group.create(Group(name="FIRST group", header="11 first group", footer="22 first group"))
+        old_groups = app.group.get_group_list()
+        index = randrange(len(old_groups))
+        app.group.delete_group_by_index(index)
+        assert len(old_groups) - 1 == app.group.count()
+        new_groups = app.group.get_group_list()
+        old_groups[index:index+1] = []  # удалили первый элемент
+        assert old_groups == new_groups
