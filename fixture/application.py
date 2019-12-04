61990acc4d8e26d9762f31e0e686cb1e634e5644
koyger <koygerov.work@gmail.com>
2019-12-04 14:23:37 +0300

    Command-line options realized: a) Browser selection b) URL selection Tested on Chrome, Firefox, IE11

--- fixture/application.py
+++ fixture/application.py
@@ -7,12 +7,21 @@ from fixture.session import SessionHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        # self.wd = webdriver.Chrome()
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+
+        self.base_url = base_url
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-        print("APP INITIALIZED")
 
     def is_valid(self):
         try:
@@ -26,7 +35,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url.endswith("/addressbook/") and len(wd.find_elements_by_xpath(
                 "//a[contains(text(),'Last name')]")) > 0):
-            self.wd.get("http://localhost/addressbook/index.php")
+            self.wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
