8d8ff6cfe140bd9b83c01f75634512a319132f07
koyger <koygerov.work@gmail.com>
2019-12-09 20:21:06 +0300

    Groups data generator created. You can set quantity and name of generated file

---
+++ generator/group.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+import random
+import string
+import getopt
+import sys
+from model.group import Group
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 2
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", header="", footer="")] + [
+    Group(name=random_string("Group ", 10), header=random_string("Header ", 12), footer=random_string("Footer ", 12))
+    for i in range(n)
+]
+
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file_path, "w") as f_out:
+    f_out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
