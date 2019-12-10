1fcc902de1234e0d1988f833f4b35c8f83aac2aa
koyger <koygerov.work@gmail.com>
2019-12-10 19:48:31 +0300

    Added test which compares lists of groups from UI and database

--- check_db_connection.py
+++ check_db_connection.py
@@ -5,7 +5,7 @@ connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="roo
 try:
     cursor = connection.cursor()
     cursor.execute("select * from group_list")
-    for row in cursor.fetchall():
+    for row in cursor:
         print(row)
 finally:
     connection.close()
---
+++ fixture/db.py
@@ -0,0 +1,26 @@
+import pymysql.cursors
+from model.group import Group
+
+
+class DbFixture:
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, database=name, user=user, password=password)
+
+    def get_group_list(self):
+        gr_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor.fetchall():
+                (gr_id, name, header, footer) = row
+                gr_list.append(Group(id=str(gr_id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return gr_list
+
+    def destroy(self):
+        self.connection.close()
