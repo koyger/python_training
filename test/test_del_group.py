6f1029f70128b0d9e8196c5d32cc448733ed02cf
koyger <koygerov.work@gmail.com>
2019-12-10 20:55:23 +0300

    Groups deletion test modified: optional UI check added

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,7 +3,7 @@ from model.group import Group
 import random
 
 
-def test_delete_some_group(app, db):
+def test_delete_some_group(app, db, check_ui):
     for i in range(2):
         if len(db.get_group_list()) == 0:
             app.group.create(Group(name="FIRST group", header="11 first group", footer="22 first group"))
@@ -14,15 +14,5 @@ def test_delete_some_group(app, db):
         assert len(old_groups) - 1 == len(new_groups)
         old_groups.remove(group)
         assert old_groups == new_groups
-#
-# def test_delete_some_group(app):
-#     for i in range(2):
-#         if app.group.count() == 0:
-#             app.group.create(Group(name="FIRST group", header="11 first group", footer="22 first group"))
-#         old_groups = app.group.get_group_list()
-#         index = randrange(len(old_groups))
-#         app.group.delete_group_by_index(index)
-#         assert len(old_groups) - 1 == app.group.count()
-#         new_groups = app.group.get_group_list()
-#         old_groups[index:index+1] = []  # удалили первый элемент
-#         assert old_groups == new_groups
+        if check_ui:
+            assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
