e99aa8652dea082746cdb042d87d791655a60af4
koyger <koygerov.work@gmail.com>
2019-12-23 19:54:40 +0300

    Modified test_add_contact_to_group for "no connections possible" situation

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -25,14 +25,22 @@ def test_add_contact_to_group(app, orm_db):
             if len(orm_db.get_contacts_not_in_group(groups[len_before-gr-1])) == 0:
                 print("GROUP NAME FULL WITH CONNs = " + groups[len_before-gr-1].name)
                 groups.pop(len_before-gr-1)
-        # если после чистки групп не осталось (или не было до), добавим группу
+        # если после чистки групп с возможностью коннекта не осталось, добавим юзера
         if len(groups) == 0:
-            groups.append(Group(name="t_a_c_t_g group", header="11 first group", footer="22 first group"))
-            # если добавлять в свежесозданную группу некого, значит совсем нет юзеров. Добавим юзера
-            if orm_db.get_contacts_not_in_group(groups[0]) == 0:
-                app.contact.create(Contact(firstname="t_a_c_t_g User", lastname="t_a_c_t_g User last name",
-                                           companyname="t_a_c_t_g user center2m", address="t_a_c_t_g User street, 21",
-                                           id=None))
+            app.contact.create(Contact(firstname="t_a_c_t_g User", lastname="t_a_c_t_g User last name",
+                                       companyname="t_a_c_t_g user center2m", address="t_a_c_t_g User street, 21"))
+            # если групп совсем нет, добавим группу
+            if len(orm_db.get_group_list) == 0:
+                app.group.create(Group(name="t_a_c_t_g Group"))
+                groups = orm_db.get_group_list()
+                # придется опять убрать группы с пустыми именами
+                for gr in range(len(groups)):
+                    print("GR = " + str(gr))
+                    print("len_before-gr-1 = " + str(len_before - gr - 1))
+                    print("GROUP NAME CHECKED = " + groups[len_before - gr - 1].name)
+                    if app.clear(groups[len_before - gr - 1].name) == "":
+                        print("GROUP NAME CLEARED = " + groups[len_before - gr - 1].name)
+                        groups.pop(len_before - gr - 1)
         print("КОЛИЧЕСТВО ГРУПП ПОСЛЕ = " + str(len(groups)))
         # теперь во все группы из groups можно добавить хотя бы одного пользователя, поэтому выберем случайную
         group = random.choice(groups)
@@ -50,7 +58,7 @@ def test_add_contact_to_group(app, orm_db):
         time.sleep(1)
         for c in range(len(cont_of_gr_list)):
             ui_id = app.contact.find_user_id_by_index(c)
-            print("Пробуем юзер айди = " + str(ui_id) +" c user_to_add.id = "+ str(user_to_add.id) + " по индексу " + str(c))
+            print("Пробуем юзер айди = "+str(ui_id)+" c user_to_add.id = "+str(user_to_add.id)+" по индексу "+str(c))
             if ui_id == user_to_add.id:
                 index = c
                 print("Подобран индекс = " + str(ui_id) + " для юзера с индексом " + str(index))
